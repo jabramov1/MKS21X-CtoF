573f7128e7507383687b8dd610b201c8defb3709
jabramov1 <90768325+jabramov1@users.noreply.github.com>
2021-09-24 01:35:57 -0400

    Add files via upload

---
+++ CtoFTester.java
@@ -0,0 +1,11 @@
+//the functions should have degrees celsuis and degrees farenheit as the parameters for the celsiusToFahrenheit and the fahrenheitToCelsius respectively
+public class CtoFTester {
+    public double  celsiusToFahrenheit(int celsuis) {
+      return (celsuis*9/5 +32);
+    }
+    public double  fahrenheitToCelsius(int farenheit){
+     return ((farenheit-32)*5/9);
+    }
+  }
+
+
